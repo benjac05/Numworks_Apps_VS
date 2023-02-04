38177ec92e5d47d3a97f20591ca0be4e9db7879a
Ben Jacobson <benjac05@icloud.com>
2023-02-04 18:49:09 -0500

    folder restructure

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-#include "display.h"
-#include "eadkpp.h"
-#include "palette.h"
+#include "includes/display.h"
+#include "includes/eadkpp.h"
+#include "includes/palette.h"
 
 extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Messages";
 extern const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
